4f24f42b837781ee83384e4b2029ff1b14f65a0f
tonythecoder1 <tonypensais@hotmail.com>
2025-10-19 18:52:48 +0100

    Update database connection to Supabase with SSL v2

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 #DATABASE_URL = "postgresql+asyncpg://postgres:12345678@localhost:5432/todo"
-DATABASE_URL = ("postgresql+asyncpg://postgres:[YOUR_PASSWORD]@db.wghpvhftdrrkfrnhzkfp.supabase.co:5432/postgres?sslmode=require")
+DATABASE_URL = ("postgresql+asyncpg://postgres:Tonyfiby16@db.wghpvhftdrrkfrnhzkfp.supabase.co:5432/postgres?sslmode=require")
 
 engine = create_async_engine(
     DATABASE_URL,
