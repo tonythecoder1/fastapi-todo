e173152dc941d2d9bff3e168c4082eea056ca80c
tonythecoder1 <tonypensais@hotmail.com>
2025-10-19 23:52:38 +0100

    Fix Supabase connection

--- database.py
+++ database.py
@@ -1,24 +1,17 @@
-import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import re
+
 
 #DATABASE_URL = "postgresql+asyncpg://postgres:12345678@localhost:5432/todo"
 
-#raw_url = "postgresql+asyncpg://postgres:Tonyfiby16@db.wghpvhftdrrkfrnhzkfp.supabase.co:5432/postgres?sslmode=require"
-#DATABASE_URL = re.sub(r"sslmode=require", "ssl=require", raw_url)
+raw_url = "postgresql+asyncpg://postgres:Tonyfiby16@db.wghpvhftdrrkfrnhzkfp.supabase.co:5432/postgres?sslmode=require"
+DATABASE_URL = re.sub(r"sslmode=require", "ssl=require", raw_url)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("No DATABASE URL ENV")
 
 engine = create_async_engine(
     DATABASE_URL,
-    connect_args={"ssl": True},  
-    pool_pre_ping=True,
-    pool_size=5,
-    max_overflow=5,
-    echo=False,                   
+    echo=True
 )
 
 AsyncSessionLocal = sessionmaker(
